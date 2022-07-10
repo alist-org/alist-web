dbc78ba27373e6417e7ab7454fbc539f2119bdeb
Noah Hsu <i@nn.ci>
2022-07-10 16:20:30 +0800

    chore: optimize `useT`

--- src/hooks/useT.ts
+++ src/hooks/useT.ts
@@ -10,7 +10,11 @@ const useT = () => {
   ) => {
     const value = t(key, params, defaultValue);
     if (!value) {
-      const last = key.slice(key.lastIndexOf(".") + 1);
+      let lastDotIndex = key.lastIndexOf(".");
+      if (lastDotIndex === key.length - 1) {
+        lastDotIndex = key.lastIndexOf(".", lastDotIndex - 1);
+      }
+      const last = key.slice(lastDotIndex + 1);
       return firstUpperCase(last).split("_").join(" ");
     }
     return value;
