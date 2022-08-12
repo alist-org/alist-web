ced27783db78e31c54fadb201628fc759ca77b58
Noah Hsu <i@nn.ci>
2022-08-12 21:59:19 +0800

    feat: copy i18n entry file

---
+++ scripts/i18n.mjs
@@ -0,0 +1,11 @@
+import fs from "fs";
+import path from "path";
+
+const root = "./src/lang";
+const entry = "entry.ts";
+const langs = fs.readdirSync(root);
+langs
+  .filter((lang) => lang !== "en")
+  .forEach((lang) => {
+    fs.copyFileSync(path.join(root, "en", entry), path.join(root, lang, entry));
+  });
