2e7bc5fb90bda32b925c866c7d4975437bf525fd
Noah Hsu <i@nn.ci>
2022-09-09 12:56:41 +0800

    ci: split build and release

---
+++ release.sh
@@ -0,0 +1,27 @@
+# replace version
+cd alist-web
+version=$(git describe --abbrev=0 --tags)
+sed -i -e "s/0.0.0/$version/g" package.json
+cat package.json
+
+# build
+pnpm install
+pnpm i18n
+pnpm build
+cp -r dist ../
+cd ..
+
+# commit to web-dist
+cd web-dist
+rm -rf dist
+cp -r ../dist .
+git add .
+git config --local user.email "i@nn.ci"
+git config --local user.name "Noah Hsu"
+git commit --allow-empty -m "upload $version dist files" -a
+git tag -a $version -m "release $version"
+cd ..
+
+mkdir compress
+tar -czvf compress/dist.tar.gz dist/*
+zip -r compress/dist.zip dist/*
