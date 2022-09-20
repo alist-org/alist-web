558a8fc7f6ae8dba6fcd2514e4ee2a0d02348a68
Noah Hsu <i@nn.ci>
2022-09-20 16:07:33 +0800

    fix: set `break-all` for file name (close alist-org/alist#1725)

--- src/pages/home/previews/info.tsx
+++ src/pages/home/previews/info.tsx
@@ -6,14 +6,21 @@ import { getIconByObj } from "~/utils/icon";
 
 export const FileInfo = (props: { children: JSXElement }) => {
   return (
-    <VStack py="$6" spacing="$6">
+    <VStack class="fileinfo" py="$6" spacing="$6">
       <Icon
         color={getMainColor()}
         boxSize="$20"
         as={getIconByObj(objStore.obj)}
       />
       <VStack spacing="$2">
-        <Heading size="lg">{objStore.obj.name}</Heading>
+        <Heading
+          size="lg"
+          css={{
+            wordBreak: "break-all",
+          }}
+        >
+          {objStore.obj.name}
+        </Heading>
         <Text color="$neutral10" size="sm">
           {getFileSize(objStore.obj.size)} · {formatDate(objStore.obj.modified)}
         </Text>
