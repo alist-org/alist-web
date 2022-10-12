bd16d935e4ebbf1ceddd38b43b25ed6980dbf5b4
Noah Hsu <i@nn.ci>
2022-10-12 16:41:56 +0800

    perf: optimize url convert
    close alist-org/alist#1986
    close alist-org/alist#1970

--- src/utils/str.ts
+++ src/utils/str.ts
@@ -1,40 +1,38 @@
 export const firstUpperCase = (str: string) => {
   if (!str || str.length === 0) {
-    return "";
+    return ""
   }
-  return str.charAt(0).toUpperCase() + str.slice(1);
-};
+  return str.charAt(0).toUpperCase() + str.slice(1)
+}
 
 export const trimLeft = (str: string, sub: string) => {
-  return str.startsWith(sub) ? str.slice(sub.length) : str;
-};
+  return str.startsWith(sub) ? str.slice(sub.length) : str
+}
 
 export function getFileSize(size: number) {
-  if (!size) return "-";
+  if (!size) return "-"
 
-  const num = 1024.0; //byte
+  const num = 1024.0 //byte
 
-  if (size < num) return size + "B";
-  if (size < Math.pow(num, 2)) return (size / num).toFixed(2) + "K"; //kb
-  if (size < Math.pow(num, 3))
-    return (size / Math.pow(num, 2)).toFixed(2) + "M"; //M
-  if (size < Math.pow(num, 4))
-    return (size / Math.pow(num, 3)).toFixed(2) + "G"; //G
-  return (size / Math.pow(num, 4)).toFixed(2) + "T"; //T
+  if (size < num) return size + "B"
+  if (size < Math.pow(num, 2)) return (size / num).toFixed(2) + "K" //kb
+  if (size < Math.pow(num, 3)) return (size / Math.pow(num, 2)).toFixed(2) + "M" //M
+  if (size < Math.pow(num, 4)) return (size / Math.pow(num, 3)).toFixed(2) + "G" //G
+  return (size / Math.pow(num, 4)).toFixed(2) + "T" //T
 }
 
 const full = (p: number) => {
-  return p < 10 ? "0" + p : p;
-};
+  return p < 10 ? "0" + p : p
+}
 
 export function formatDate(dateStr: string) {
-  const date = new Date(dateStr);
-  const year = date.getFullYear();
-  const mon = date.getMonth() + 1;
-  const day = date.getDate();
-  const hour = date.getHours();
-  const min = date.getMinutes();
-  const sec = date.getSeconds();
+  const date = new Date(dateStr)
+  const year = date.getFullYear()
+  const mon = date.getMonth() + 1
+  const day = date.getDate()
+  const hour = date.getHours()
+  const min = date.getMinutes()
+  const sec = date.getSeconds()
 
   return (
     year +
@@ -48,42 +46,53 @@ export function formatDate(dateStr: string) {
     full(min) +
     ":" +
     full(sec)
-  );
+  )
 }
 
 export const convertURL = (scheme: string, url: string, name: string) => {
-  let ans = scheme;
-  ans = ans.replace("$name", name);
-  ans = ans.replace("$url", url);
-  ans = ans.replace("$e_url", encodeURIComponent(url));
-  ans = ans.replace("$b_url", window.btoa(url));
-  ans = ans.replace("$eb_url", encodeURIComponent(window.btoa(url)));
-  return ans;
-};
+  let ans = scheme
+  ans = ans.replace("$name", name)
+  const regex = /\$[eb_]*url/g
+  ans = ans.replace(regex, (old) => {
+    const op = old.match(/e|b/g)
+    let u = url
+    if (op) {
+      for (const o of op.reverse()) {
+        if (o === "e") {
+          u = encodeURIComponent(u)
+        } else if (o === "b") {
+          u = window.btoa(u)
+        }
+      }
+    }
+    return u
+  })
+  return ans
+}
 
 export const strToRegExp = (str: string) => {
-  str = str.trim();
-  let pattern = str.replace(/^\/(.*)\/([a-z]*)$/, "$1");
-  let args = str.replace(/^\/(.*)\/([a-z]*)$/, "$2");
-  const reg = new RegExp(pattern, args);
-  return reg;
-};
+  str = str.trim()
+  let pattern = str.replace(/^\/(.*)\/([a-z]*)$/, "$1")
+  let args = str.replace(/^\/(.*)\/([a-z]*)$/, "$2")
+  const reg = new RegExp(pattern, args)
+  return reg
+}
 
 const ENC = {
   "+": "-",
   "/": "_",
   "=": ".",
-};
+}
 const DEC = {
   "-": "+",
   _: "/",
   ".": "=",
-};
+}
 
 export const safeBase64 = (base64: string) => {
-  return base64.replace(/[+/=]/g, (m) => ENC[m as "+" | "/" | "="]);
-};
+  return base64.replace(/[+/=]/g, (m) => ENC[m as "+" | "/" | "="])
+}
 
 export const safeBtoa = (str: string) => {
-  return safeBase64(window.btoa(str));
-};
+  return safeBase64(window.btoa(str))
+}
