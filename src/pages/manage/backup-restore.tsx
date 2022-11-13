42fc1df99fa776ce8c44201d85e0501931145b3e
itsHenry35 <2671230065@qq.com>
2022-11-13 13:08:02 +0800

    test

--- src/pages/manage/backup-restore.tsx
+++ src/pages/manage/backup-restore.tsx
@@ -1,5 +1,7 @@
 import { Box, Button, notificationService } from "@hope-ui/solid";
-import {r} from "~/utils";
+import {r, handleResp} from "~/utils";
+import { useFetch} from "~/hooks";
+import { Meta, PageResp , Storage, SettingItem, User, EmptyResp} from "~/types";
 
 const BackupRestore = () => {
   return (
@@ -23,11 +25,12 @@ const BackupRestore = () => {
         
 };
 
+
 function download(filename: string, data: any) {
-  const blob = new Blob([JSON.stringify(data,null,2)], {
-    type: "application/json",
-  });
-  const url = URL.createObjectURL(blob);
+  console.log(data)
+  console.log(JSON.stringify(data, null, 2))
+  const file = new Blob([JSON.stringify(data, null, 2)], { type: "application/json" });
+  const url = URL.createObjectURL(file);
   const a = document.createElement("a");
   a.href = url;
   a.download = filename;
@@ -36,27 +39,137 @@ function download(filename: string, data: any) {
 }
 
 const backup = async() => {
+  const data = {
+    "setting": [],
+    "users": [],
+    "storages": [],
+    "metas": [],
+  }
   const date = new Date();
   const year = date.getFullYear();
   const month = date.getMonth() + 1;
   const dates = date.getDate();
-
   const todaysdate = year + "-" + month + "-" + dates;
-  const data = r.get("/admin/setting/list");
-  if ((await data).code == 200)
-  {
-    download("alist_backup_" + todaysdate + ".json", (await data).data);
+  let all_success = 1
+
+  const [, getSettings] = useFetch(() => r.get("/admin/setting/list"));
+  const setSettings = async () => {
+    const resp: PageResp<SettingItem> = await getSettings();
+    handleResp(resp, () => {
+      if (resp.code == 200)
+      {
+        notificationService.show({
+          title: "Settings Backup Succeeded~",
+          description: "Settings Backup Succeeded~",
+          status: "success",
+        })
+        data.setting = resp.data
+      }
+      else
+      {
+        notificationService.show({
+          title: "Settings Backup Failed~",
+          description: resp.message,
+          status: "danger",
+        })
+        all_success = 0
+      }
+    });
+  };
+  setSettings();
+
+  const [, getUsers] = useFetch(() => r.get("/admin/user/list"));
+  const setUsers = async () => {
+    const resp: PageResp<User> = await getUsers();
+    handleResp(resp, () => {
+      if (resp.code == 200)
+      {
+        notificationService.show({
+          title: "Users Backup Succeeded~",
+          description: "Users Backup Succeeded~",
+          status: "success",
+        })
+        data.users = resp.data
+      }
+      else
+      {
+        notificationService.show({
+          title: "Users Backup Failed~",
+          description: resp.message,
+          status: "danger",
+        })
+        all_success = 0
+      }
+    });
+  };
+  setUsers();
+
+  const [, getMetas] = useFetch(() => r.get("/admin/meta/list"));
+  const setmetas = async () => {
+    const resp: PageResp<Meta> = await getMetas();
+    handleResp(resp, () => {
+      console.log(resp)
+      if (resp.code == 200)
+      {
+        notificationService.show({
+          title: "Metas Backup Succeeded~",
+          description: "Metas Backup Succeeded~",
+          status: "success",
+        })
+        data.metas = resp.data
+      }
+      else
+      {
+        notificationService.show({
+          title: "Metas Backup Failed~",
+          description: resp.message,
+          status: "danger",
+        })
+        all_success = 0
+      }
+    });
+  };
+  setmetas();
+
+  const [, getStorages] = useFetch(() => r.get("/admin/storage/list"));
+  const setStorages = async () => {
+    const resp: PageResp<Storage> = await getStorages();
+    handleResp(resp, () => {
+      if (resp.code == 200)
+      {
+        notificationService.show({
+          title: "Storages Backup Succeeded~",
+          description: "Storages Backup Succeeded~",
+          status: "success",
+        })
+        data.storages = resp.data
+      }
+      else
+      {
+        notificationService.show({
+          title: "Storages Backup Failed~",
+          description: resp.message,
+          status: "danger",
+        })
+        all_success = 0
+      }
+    });
+  };
+  setStorages();
+
+
+if (all_success == 1) {
+    download("alist_backup_" + todaysdate + ".json", data);
     notificationService.show({
-      title: "Backup Succeeded~",
-      description: "Backup Succeeded~",
+      title: "Backup Downloaded Successfully~",
+      description: "Backup Downloaded Successfully~",
       status: "success",
     })
   }
-  else
-  {
+else {
     notificationService.show({
-      title: "Backup Failed~",
-      description: data.message,
+      title: "Failed to Backup~",
+      description: "Failed to Backup~",
       status: "danger",
     })
   }
@@ -73,7 +186,6 @@ const restore = async() => {
     reader.onload = async (e) => {
       const data = JSON.parse(e.target.result as string);
       const resp = await r.post("/admin/setting/save", data);
-      console.log(resp);
       if (resp.code == 200)
       {
         notificationService.show({
