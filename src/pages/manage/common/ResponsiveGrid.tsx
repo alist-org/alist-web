293f1732615e730f2c48cfa480d73288df304afe
Noah Hsu <i@nn.ci>
2022-12-20 20:05:20 +0800

    feat: customize settings layout

--- src/pages/manage/common/ResponsiveGrid.tsx
+++ src/pages/manage/common/ResponsiveGrid.tsx
@@ -1,14 +1,20 @@
 import { Grid } from "@hope-ui/solid"
 import { JSXElement } from "solid-js"
+import { getSetting } from "~/store"
 
 export const ResponsiveGrid = (props: { children: JSXElement }) => {
+  const layout = getSetting("settings_layout")
+  let v
+  if (layout === "responsive") {
+    v = "repeat(auto-fill, minmax(424px, 1fr))"
+  }
   return (
     <Grid
       w="$full"
       gap="$2"
       templateColumns={{
         "@initial": "1fr",
-        "@lg": "repeat(auto-fill, minmax(424px, 1fr))",
+        "@lg": v,
       }}
     >
       {props.children}
