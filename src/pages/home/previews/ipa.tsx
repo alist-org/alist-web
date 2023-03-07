52f4562f300d2df5b410d40ded2d0a8d260f9dba
anwen-anyi <anwen_anyi@foxmail.com>
2023-03-07 13:23:31 +0800

    ipa add HStack

--- src/pages/home/previews/ipa.tsx
+++ src/pages/home/previews/ipa.tsx
@@ -10,34 +10,36 @@ const Ipa = () => {
   const [installing, setInstalling] = createSignal(false)
   return (
     <FileInfo>
-      <Button
-        as="a"
-        href={
-          "itms-services://?action=download-manifest&url=" +
-          `${api}/i/${safeBtoa(
-            encodeURIComponent(objStore.raw_url) +
-            "/" +
-            baseName(encodeURIComponent(objStore.obj.name))
-          )}.plist`
-        }
-        onClick={() => {
-          setInstalling(true)
-        }}
-      >
-        {t(`home.preview.${installing() ? "installing" : "install"}`)}
-      </Button>
-      <Button
-        as="a"
-        colorScheme="primary"
-        href={
-          "apple-magnifier://install?url=" + encodeURIComponent(useLink().rawLink(objStore.obj, true))
-        }
-        onClick={() => {
-          setInstalling(true)
-        }}
-      >
-        {t(`home.preview.${installing() ? "tr-installing" : "tr-install"}`)}
-      </Button>
+      <HStack spacing="$2">
+        <Button
+          as="a"
+          href={
+            "itms-services://?action=download-manifest&url=" +
+            `${api}/i/${safeBtoa(
+              encodeURIComponent(objStore.raw_url) +
+              "/" +
+              baseName(encodeURIComponent(objStore.obj.name))
+            )}.plist`
+          }
+          onClick={() => {
+            setInstalling(true)
+          }}
+        >
+          {t(`home.preview.${installing() ? "installing" : "install"}`)}
+        </Button>
+        <Button
+          as="a"
+          colorScheme="primary"
+          href={
+            "apple-magnifier://install?url=" + encodeURIComponent(useLink().rawLink(objStore.obj, true))
+          }
+          onClick={() => {
+            setInstalling(true)
+          }}
+        >
+          {t(`home.preview.${installing() ? "tr-installing" : "tr-install"}`)}
+        </Button>
+      </HStack>
     </FileInfo>
   )
 }
