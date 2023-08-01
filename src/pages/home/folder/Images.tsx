912e0dbf040628bdaecad13d7f0a3b0d7b06d78c
Andy Hsu <i@nn.ci>
2023-08-01 15:41:58 +0800

    fix: folder not shows in image view if no images
    close alist-org/alist#4886

--- src/pages/home/folder/Images.tsx
+++ src/pages/home/folder/Images.tsx
@@ -1,10 +1,13 @@
-import { Flex, Grid, VStack } from "@hope-ui/solid"
+import { Flex, Grid, Heading, VStack } from "@hope-ui/solid"
 import { For, Show, createMemo } from "solid-js"
 import { ImageItem } from "./ImageItem"
 import { local, objStore } from "~/store"
 import { GridItem } from "./GridItem"
+import { StoreObj } from "~/types"
+import { useT } from "~/hooks"
 
-const ImageLayout = () => {
+const ImageLayout = (props: { images: StoreObj[] }) => {
+  const t = useT()
   const folders = createMemo(() => (
     <Grid
       w="$full"
@@ -24,13 +27,18 @@ const ImageLayout = () => {
       <Show when={local["show_folder_in_image_view"] === "top"}>
         {folders()}
       </Show>
-      <Flex w="$full" gap="$1" flexWrap="wrap" class="image-images">
-        <For each={objStore.objs}>
-          {(obj, i) => {
-            return <ImageItem obj={obj} index={i()} />
-          }}
-        </For>
-      </Flex>
+      <Show
+        when={props.images.length > 0}
+        fallback={<Heading m="$2">{t("home.no_images")}</Heading>}
+      >
+        <Flex w="$full" gap="$1" flexWrap="wrap" class="image-images">
+          <For each={objStore.objs}>
+            {(obj, i) => {
+              return <ImageItem obj={obj} index={i()} />
+            }}
+          </For>
+        </Flex>
+      </Show>
       <Show when={local["show_folder_in_image_view"] === "bottom"}>
         {folders()}
       </Show>
