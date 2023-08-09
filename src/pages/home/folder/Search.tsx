dc09b46c56d08c4131c047f7f47f1b2fe8b61c22
itsHenry <2671230065@qq.com>
2023-08-09 14:51:21 +0800

    fix: incorrect current page of search paginator (#106)
    close alist-org/alist#4974
    Co-authored-by: Andy Hsu <i@nn.ci>

--- src/pages/home/folder/Search.tsx
+++ src/pages/home/folder/Search.tsx
@@ -178,6 +178,7 @@ const SearchResult = (props: { node: SearchNode; keywords: string }) => {
 }
 
 const Search = () => {
+  const pageSize = 100
   const { isOpen, onOpen, onClose, onToggle } = createDisclosure()
   const t = useT()
   const handler = (name: string) => {
@@ -219,6 +220,7 @@ const Search = () => {
       password(),
       scope(),
       page,
+      pageSize,
     )
     handleResp(resp, (data) => {
       const content = data.content
@@ -304,15 +306,13 @@ const Search = () => {
                 {(item) => <SearchResult node={item} keywords={keywords()} />}
               </For>
             </VStack>
-            <Show when={data().total > 0}>
-              <Paginator
-                total={data().total}
-                defaultPageSize={100}
-                onChange={(page) => {
-                  search(page)
-                }}
-              />
-            </Show>
+            <Paginator
+              total={data().total}
+              defaultPageSize={pageSize}
+              onChange={(page) => {
+                search(page)
+              }}
+            />
           </VStack>
         </ModalBody>
       </ModalContent>
