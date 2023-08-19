06148660c80341af0f48dead0c57b039b21c1c8a
Andy Hsu <i@nn.ci>
2023-08-19 14:02:57 +0800

    fix: missing files on traversing folder (close alist-org/alist#5034)
    Caused by 43adecdbc945e2a194bc98f7528eb7b71167c95f

--- src/pages/home/uploads/util.ts
+++ src/pages/home/uploads/util.ts
@@ -19,12 +19,28 @@ export const traverseFileTree = async (entry: FileSystemEntry) => {
         }, errorCallback)
       } else if (entry.isDirectory) {
         const dirReader = (entry as FileSystemDirectoryEntry).createReader()
-        dirReader.readEntries(async (entries) => {
-          for (let i = 0; i < entries.length; i++) {
-            await internalProcess(entries[i], path + entry.name + "/")
-          }
-          resolve({})
-        }, errorCallback)
+        const readEntries = () => {
+          dirReader.readEntries(async (entries) => {
+            for (let i = 0; i < entries.length; i++) {
+              await internalProcess(entries[i], path + entry.name + "/")
+            }
+            resolve({})
+            /**
+            why? https://stackoverflow.com/questions/3590058/does-html5-allow-drag-drop-upload-of-folders-or-a-folder-tree/53058574#53058574
+            Unfortunately none of the existing answers are completely correct because 
+            readEntries will not necessarily return ALL the (file or directory) entries for a given directory. 
+            This is part of the API specification (see Documentation section below).
+            
+            To actually get all the files, we'll need to call readEntries repeatedly (for each directory we encounter) 
+            until it returns an empty array. If we don't, we will miss some files/sub-directories in a directory 
+            e.g. in Chrome, readEntries will only return at most 100 entries at a time.
+            */
+            if (entries.length > 0) {
+              readEntries()
+            }
+          }, errorCallback)
+        }
+        readEntries()
       }
     })
     await promise
