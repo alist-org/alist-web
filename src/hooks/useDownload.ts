00010eff9012e3f94521418b630643216ba9970b
qilme <89187439+qilme@users.noreply.github.com>
2024-05-05 16:29:32 +0800

    refactor: update download filename to match selected object name

--- src/hooks/useDownload.ts
+++ src/hooks/useDownload.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 import { local, password, selectedObjs as _selectedObjs } from "~/store"
-import { fsList, notify, pathJoin } from "~/utils"
+import { fsList, notify, pathBase, pathJoin } from "~/utils"
 import { getLinkByDirAndObj, useRouter, useT } from "~/hooks"
 import { useSelectedLink } from "~/hooks"
 import { Obj } from "~/types"
@@ -164,7 +164,15 @@ export const useDownload = () => {
       a.href = URL.createObjectURL(
         new Blob([rawLinksText(true)], { type: "application/x-mpegURL" }),
       )
-      a.download = "playlist.m3u8"
+      const selectedObjs = _selectedObjs()
+      let saveName = pathBase(pathname())
+      if (selectedObjs.length === 1) {
+        saveName = selectedObjs[0].name
+      }
+      if (!saveName) {
+        saveName = t("manage.sidemenu.home")
+      }
+      a.download = `${saveName}.m3u8`
       a.click()
       URL.revokeObjectURL(a.href)
     },
