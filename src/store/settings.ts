118aa854a19933a33b7ecf04383e6b9db0896753
快乐的老鼠宝宝 <keaitianxinmail@qq.com>
2024-06-28 23:47:53 +0800

    docs: change outdated repository link to alist-org (#142)

--- src/store/settings.ts
+++ src/store/settings.ts
@@ -8,7 +8,7 @@ export const setSettings = (items: Record<string, string>) => {
   })
   const version = settings["version"] || "Unknown"
   console.log(
-    `%c AList %c ${version} %c https://github.com/Xhofe/alist`,
+    `%c AList %c ${version} %c https://github.com/alist-org/alist`,
     "color: #fff; background: #5f5f5f",
     "color: #fff; background: #70c6be",
     "",
