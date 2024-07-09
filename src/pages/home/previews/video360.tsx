fe6957cdf9c9a298fe59e08cb25ef820b00086a9
22 <60903333+nini22P@users.noreply.github.com>
2024-07-09 21:30:46 +0800

    move canvas and video

--- src/pages/home/previews/video360.tsx
+++ src/pages/home/previews/video360.tsx
@@ -12,57 +12,53 @@ const Preview = () => {
     videos = [objStore.obj]
   }
 
-  let view360Instance: View360
+  let viewer: View360
 
   onMount(() => {
-    const container = document.getElementById("video360-player")
-    const div = document.createElement("div")
-    div.style.position = "relative"
-    div.style.height = "100%"
-    div.style.width = "100%"
+    const container = document.getElementById("view360-container")
+    const video = document.getElementById("view360-video")
 
-    const canvas = document.createElement("canvas")
-    canvas.className = "view360-canvas"
-
-    container?.appendChild(div)
-    div.appendChild(canvas)
-
-    view360Instance = new View360(div, {
-      projection: new EquirectProjection({
-        src: objStore.raw_url,
-        video: {
-          autoplay: false,
-          muted: false,
-        },
-      }),
-      plugins: [
-        new ControlBar({
-          pieView: { order: 0 },
-          gyroButton: { position: "top-right", order: 1 },
-          vrButton: { position: "top-right", order: 2 },
+    if (container && video) {
+      viewer = new View360(container, {
+        projection: new EquirectProjection({
+          src: video,
+          video: {},
         }),
-      ],
-    })
-  })
-
-  onCleanup(() => {
-    if (view360Instance) {
-      view360Instance.destroy()
-    }
-
-    const container = document.getElementById("video360-player")
-    if (container) {
-      while (container.firstChild) {
-        container.removeChild(container.firstChild)
-      }
+        plugins: [
+          new ControlBar({
+            pieView: { order: 0 },
+            gyroButton: { position: "top-right", order: 1 },
+            vrButton: { position: "top-right", order: 2 },
+          }),
+        ],
+      })
     }
   })
+
+  onCleanup(() => viewer?.destroy())
 
   const [autoNext, setAutoNext] = createSignal(false)
 
   return (
     <VideoBox onAutoNextChange={setAutoNext}>
-      <Box w="$full" h="60vh" id="video360-player" />
+      <Box w="$full" h="60vh" id="video360-player">
+        <Box
+          w={"100%"}
+          h={"100%"}
+          id="view360-container"
+          className="view360-container"
+        >
+          <canvas class="view360-canvas" />
+          <video
+            src={objStore.raw_url}
+            id="view360-video"
+            autoplay
+            playsinline
+            crossOrigin="anonymous"
+            style="display: none;"
+          />
+        </Box>
+      </Box>
     </VideoBox>
   )
 }
